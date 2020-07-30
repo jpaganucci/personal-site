7dc6957a4341d384e3429abb09289102a74daacf
Kyle Lennon <kylelennon@Kyles-MacBook-Pro.local>
2020-07-29 21:16:03 -0400

    Add menu toggle feature to mobile site

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,29 +1,34 @@
 <template>	
 	<div class="navbar is-white" role="navigation" aria-label="main navigation">
-	  <div class="navbar-brand">
-	    <nuxt-link to="/">
-	      <img src="logo-initials.pdf" width=80 height=114 class="navbar-item">
-	    </nuxt-link>
+		<div class="navbar-brand">
+	  		<nuxt-link to="/">
+	      			<img src="logo-initials.pdf" width=80 height=114 class="navbar-item">
+	    		</nuxt-link>
 
-	    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-	      <span aria-hidden="true"></span>
-	      <span aria-hidden="true"></span>
-	      <span aria-hidden="true"></span>
-	    </a>
-	  </div>
+	    		<a 
+				role="button" 
+				:class="['navbar-burger burger', {'is-active': isActive}]" 
+				aria-label="menu" 
+				aria-expanded="false" 
+				data-target="navbarBasicExample"
+				v-on:click="menuToggle"
+			>
+	      			<span aria-hidden="true"></span>
+	      			<span aria-hidden="true"></span>
+	      			<span aria-hidden="true"></span>
+	    		</a>
+	  	</div>
 
-	  <div id="navbarBasicExample" class="navbar-menu">
-	    <div class="navbar-end">
-	      <nuxt-link
-			class="navbar-item"
-			v-for="(slug,name) in sectionNames"
-			:key="slug"
-			:to="'/'+slug"
-		>{{ name }}</nuxt-link>
-	      </div>
-	    </div>
-
-	  </div>
+	  	<div id="navbarBasicExample" class="navbar-menu">
+	    		<div class="navbar-end">
+	      			<nuxt-link
+					class="navbar-item"
+					v-for="(slug,name) in sectionNames"
+					:key="slug"
+					:to="'/'+slug"
+				>{{ name }}</nuxt-link>
+	      		</div>
+	    	</div>
 	</div>
 </template>
 
@@ -31,8 +36,14 @@
 export default {
 	data() {
 		return {
+			isActive: false,
 			sectionNames: process.env.navItems
-		}
+		};
+	},
+	methods: {
+		menuToggle: function() {
+			this.isActive = !this.isActive;
+		}	
 	}
 } 
 
